47b0a655d6f0824bf4a0de3123a2d9e5010de70e
Tjcarroll1 <tjcarroll1@icloud.com>
2025-01-11 21:13:55 +1000

    added success page

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,14 +1,30 @@
-'use client';
+"use client";
 
 const ThankYouPage = () => {
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-4">
-      
-        <div className="text-center max-w-lg bg-white rounded-lg shadow-lg p-10">
-          <h1 className="text-4xl text-green-500 mb-4">Thank you for your purchase!</h1>
-          <p className="text-xl text-gray-700 mb-6">Your purchase has been successfully recorded.</p>
-        </div>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center bg-no-repeat p-4"
+      style={{
+        backgroundImage: `url('/images/pool.jpg')`,
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        backgroundBlend: "overlay",
+      }}
+    >
+      <div
+        className="text-center max-w-lg bg-white/90 backdrop-blur-sm rounded-lg shadow-lg p-10"
+        style={{ backdropFilter: "blur(8px)" }}
+      >
+        <h1 className="text-4xl text-dark-teal mb-4">
+          Thank you for your purchase!
+        </h1>
+        <p className="text-xl text-gray-700 mb-6">
+          Please wait while we redirect you to the course page.
+          <br></br>
+          <br></br>
+          You will need to create an account with the same email address you
+          used to purchase the course.
+        </p>
+      </div>
     </div>
   );
 };
